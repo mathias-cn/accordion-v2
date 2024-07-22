dbdd3b1b80e3d6f8a3527672833c8bf66efe7899
Mathias Navarre <mathiascnavarre@gmail.com>
2024-07-22 18:55:51 -0300

    Final version of v2

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,33 @@
+import { useState } from "react"
 import { AccordionItem } from "./components/accordion"
 
 interface AccordionObj {
   id: number
   question: string
   answer: string
-  opened: boolean
 }
 
 export function App() {
+  const [openAccordionItem, setOpenAccordionItem] = useState<null | number>(null)
+  function handleAccordionItemClick(id: number) {
+    setOpenAccordionItem(id)
+  }
+
   const questions: AccordionObj[] = [
     {
       id: 1,
       question: "What is the origin of soccer?",
       answer: "Soccer originated in England in the mid-19th century, with the first standardized rules established by The Football Association in 1863.",
-      opened: false
     },
     {
       id: 2,
       question: "How many players are on a soccer team?",
       answer: "A standard soccer team has 11 players, including one goalkeeper and ten outfield players.",
-      opened: false
     },
     {
       id: 3,
       question: "What is the offside rule in soccer?",
       answer: "A player is offside if they are nearer to the opponent's goal than the ball and the second-to-last defender when the ball is passed to them and are involved in active play.",
-      opened: false
     }
   ]
 
@@ -34,9 +36,11 @@ export function App() {
       {questions.map((a, index) => 
         (
           <AccordionItem 
-            key={index}
+            key={a.id}
             index={index + 1}
             accordionObj={a}
+            opened={openAccordionItem === a.id}
+            onClickFunction={handleAccordionItemClick}
           />
         )
       )}
--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -1,29 +1,22 @@
-import { useState } from "react"
-
 interface AccordionProps {
     index: number
     accordionObj: {
         id: number
         question: string,
-        answer: string,
-        opened: boolean
+        answer: string
     }
+    opened: boolean
+    onClickFunction: (id: number) => void
 }
 
-export function AccordionItem({ index, accordionObj }: AccordionProps) {
-    const [isOpen, setIsOpen] = useState(false)
+export function AccordionItem({ index, accordionObj, opened, onClickFunction }: AccordionProps) {
+    let indexToDisplay: number | string = index
+    if(indexToDisplay < 10) indexToDisplay = "0" + index
 
-    function handleClick() {
-        setIsOpen(!isOpen)
-    }
-
-    let newIndex: number | string = index
-    if(newIndex < 10) newIndex = "0" + index
-
-    if(isOpen) {
+    if(opened) {
         return (
-            <div onClick={handleClick} className="cursor-pointer border-t-8 border-green-600 font-semibold py-4 px-8 bg-gray-100 rounded-lg w-full flex gap-6 items-start justify-between text-2xl text-left">
-                <span className="text-green-600">{newIndex}</span>
+            <div onClick={() => onClickFunction(accordionObj.id)} className="cursor-pointer border-t-8 border-green-600 font-semibold py-4 px-8 bg-gray-100 rounded-lg w-full flex gap-6 items-start justify-between text-2xl text-left">
+                <span className="text-green-600">{indexToDisplay}</span>
                 <div className="flex-1 space-y-4">
                     <h2 className="text-green-600">{accordionObj.question}</h2>
                     <p className="text-lg font-normal">{accordionObj.answer}</p>
@@ -33,8 +26,8 @@ export function AccordionItem({ index, accordionObj }: AccordionProps) {
         )
     }
     return (
-        <div onClick={handleClick} className="cursor-pointer font-semibold py-4 px-8 bg-gray-100 rounded-lg w-full flex gap-6 items-start justify-between text-2xl text-left">
-            <span>{newIndex}</span>
+        <div onClick={() => onClickFunction(accordionObj.id)} className="cursor-pointer font-semibold py-4 px-8 bg-gray-100 rounded-lg w-full flex gap-6 items-start justify-between text-2xl text-left">
+            <span>{indexToDisplay}</span>
             <div className="flex-1">
                 <h2>{accordionObj.question}</h2>
             </div>
